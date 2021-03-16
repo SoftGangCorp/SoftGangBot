9dda84b8413a96d1722629666f76cabc9d4cac52
Atlas <nabeel.meyers.06@gmail.com>
2021-03-16 12:51:29 +0200

    random word generator

---
+++ src/main/java/io/softgang/model/WordData.java
@@ -0,0 +1,22 @@
+package io.softgang.model;
+
+public class WordData {
+    private String id;
+    private WordIndexData data;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public WordIndexData getData() {
+        return data;
+    }
+
+    public void setData(WordIndexData data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/io/softgang/model/WordIndexData.java
@@ -0,0 +1,24 @@
+package io.softgang.model;
+
+public class WordIndexData {
+    private String[] name;
+    private ExplainationData[] explanation;
+
+
+    public String[] getName() {
+        return name;
+    }
+
+    public void setName(String[] name) {
+        this.name = name;
+    }
+
+    public ExplainationData[] getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(ExplainationData[] explanation) {
+        this.explanation = explanation;
+    }
+}
+
