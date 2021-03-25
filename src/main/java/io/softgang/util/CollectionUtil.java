282e667e59c0fab46b9ac29c9e1b02a3b3a3fe76
Shyam Prag <pragshyam11@gmail.com>
2021-03-25 11:46:51 +0200

    updated profanity measures

---
+++ src/main/java/io/softgang/util/CollectionUtil.java
@@ -0,0 +1,14 @@
+package io.softgang.util;
+
+import java.util.Arrays;
+
+public class CollectionUtil {
+    public static boolean stringContainsItemFromList(String inputStr, String[] items) {
+        inputStr = inputStr != null ? inputStr : " ";
+        String [] inputStrings = inputStr.split(" ");
+        return Arrays.stream(inputStrings)
+                .anyMatch(word -> Arrays.stream(items)
+                        .anyMatch(item -> item.contains(word.toLowerCase().trim())));
+    }
+}
+
